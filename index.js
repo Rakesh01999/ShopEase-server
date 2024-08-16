45ee7eac30fd86fd61db7f5f6430a12f39c33d76
Rakesh Biswas <rbiswas01999@gmail.com>
2024-08-16 21:50:23 +0600

    file modified

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.use(cors({
 app.use(express.json());
 
 
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uuibjb3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
